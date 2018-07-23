773986ec6c73d233e144d6d54b7a0f05d27481bf
Aaron Perry <atpx15@gmail.com>
2018-07-23 12:05:07 -0500

    complete

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "SerialPort.h"
+#include "rs232.h"
 #include <stdio.h>
 #include <string.h>
 #include <fstream>
@@ -7,30 +7,33 @@
 
 using namespace std;
 
-#define MAX_DATA_LENGTH 1
 
-char incomingData[MAX_DATA_LENGTH];
 
-//Arduino SerialPort object
-SerialPort *arduino;
+#define COMPORT			24 		// this is '/dev/ttyUSB0' (for arduino) or choose wherever you added '/dev/ttyAMA0' (for raspberry pi UART pins) to the list
+#define BAUDRATE		500000	// or whatever baudrate you want
+#define RECEIVE_CHARS	1		// or whatever size of buffer you want to receive
+
+unsigned char incomingData[RECEIVE_CHARS];
+
 
 void ReceiveData(ofstream& filename)
 {
-    int readResult = arduino->readSerialPort(incomingData, MAX_DATA_LENGTH);     
-    if (readResult >= 1){
-        filename.write(incomingData,sizeof(incomingData));
+    int readResult = RS232_PollComport(COMPORT, incomingData, RECEIVE_CHARS);  
+    if (readResult >= 1){  
+        filename.write((const char*)incomingData,sizeof(incomingData));
     }
 }
 
 int main()
 {    
     
-    //Read in the port name from file: 
+   /* //Read in the port name from file: 
     std::ifstream conFile ("rpi_serial.conf");
     char inString[100]; 
     conFile >> inString; 
     conFile.close();
     char* portName = inString; 
+*/ 
 
     //sequencing file numbers: 
     std::ifstream seqFile ("sequence.dat", std::ifstream::in); 
@@ -49,15 +52,16 @@ int main()
     std::ofstream logfile (fileName,std::ofstream::binary);
      
     //Open up the serial connection: 
-    arduino = new SerialPort(portName);
+    RS232_OpenComport(COMPORT, BAUDRATE,"8N1");
 
-    //Checking if Arduino is connected or not:
+    /*//Checking if Arduino is connected or not:
     if (arduino->isConnected()){
         std::cout << "Connection established at port " << portName << endl;
     }
+    */
     
     //Log the ADC data:
-    while(arduino->isConnected()){   
+    while(1){   
         ReceiveData(logfile);
     }
     
